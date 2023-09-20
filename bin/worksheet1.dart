ecc3c50340fa7dfa7a31956d7c48620f606703cb
Salmaankg <salmankg123@gmail.com>
2023-09-20 21:43:12 +0530

    initial commit

---
+++ bin/worksheet1.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main(){
+  print("enter your name");
+  String? name =stdin.readLineSync();
+  print("enter your age ");
+  int age =int.parse(stdin.readLineSync()!);
+  print("enter your cgpa");
+  double cgpa =double.parse(stdin.readLineSync()!);
+  print("enter your phone");
+  int phone =int.parse(stdin.readLineSync()!);
+  print("enter your email");
+  String? email =stdin.readLineSync();
+
+  stdout.writeln("Name : $name");
+  stdout.writeln("age : $age");
+  stdout.writeln("mark : $cgpa");
+  stdout.writeln("phone : $phone");
+  stdout.writeln("email : $email");
+
+}
