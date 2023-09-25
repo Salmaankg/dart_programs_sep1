34c937efebe48f6089e57b4a8aee2deb12cc77c7
Salmaankg <salmankg123@gmail.com>
2023-09-25 14:19:00 +0530

    initial commit

--- bin/programs/sum_of_even_and_odd_1_to_10.dart
+++ bin/programs/sum_of_even_and_odd_1_to_10.dart
@@ -1,3 +1,14 @@
 void main(){
-
+int n = 10;
+  int sumOdd=0;
+  int sumEven=0;
+  for(int i =1;i<=n;i++){
+    if(i%2==0){
+      sumEven += i;
+    }else{
+      sumOdd += i;
+    }
+  }
+  print("sum of even numbers = $sumEven");
+  print("sum of oddn numbers = $sumOdd");
 }
